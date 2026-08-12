0ebf72de7a634b63ced4cc4f43363847cad7bcb2
Elise Roberts <101602267+rivel5@users.noreply.github.com>
2026-08-12 11:00:06 -0400

    Cache Chronicle gamification assets offline

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='chronicle-v2';const ASSETS=['./','index.html','styles.css','lessons-bahamian.js','lessons-caribbean.js','lessons-world.js','lessons-american.js','app.js','manifest.webmanifest','icons/icon.svg'];
+const CACHE='chronicle-v4';
+const ASSETS=['./','index.html','styles.css','gamification.css','app.js','gamification.js','lessons-bahamian.js','lessons-caribbean.js','lessons-world.js','lessons-american.js','manifest.webmanifest','icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
