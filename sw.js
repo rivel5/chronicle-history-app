372541f1a0019a568bbaf56e9530c917c6041c67
Elise Roberts <101602267+rivel5@users.noreply.github.com>
2026-08-12 10:32:57 -0400

    Update Chronicle offline cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chronicle-v1';const ASSETS=['./','index.html','styles.css','lessons.js','app.js','manifest.webmanifest','icons/icon.svg'];
+const CACHE='chronicle-v2';const ASSETS=['./','index.html','styles.css','lessons-bahamian.js','lessons-caribbean.js','lessons-world.js','lessons-american.js','app.js','manifest.webmanifest','icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
